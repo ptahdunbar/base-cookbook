0b045d4aeee29a7b786f9528dac9cf2279fdf748
Pirate Dunbar <yarr@piratedunbar.com>
2013-11-18 19:03:31 -0500

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,12 +1,12 @@
-default[:base]['user']					= 'vagrant'
-default[:base]['group']					= 'vagrant'
+default[:base]['user']                      = 'vagrant'
+default[:base]['group']					    = 'vagrant'
 
-default[:base]['dependencies']			= %w()
-default[:base]['users'] 				= []
+default[:base]['dependencies']			    = %w()
+default[:base]['users'] 				    = []
 
-default[:tz] 							= 'America/New_York'
-default[:chef_environment] 				= 'production'
+default[:tz] 							    = 'America/New_York'
+default[:environment] 				        = 'production'
 
-default[:base][:ssh_known_hosts] 		= %w(github.com)
-default[:base][:authorized_keys] 		= []
-default[:base][:github_authorized_keys]	= []
+default[:base][:ssh_known_hosts] 		    = %w(github.com)
+default[:base][:authorized_keys] 		    = []
+default[:base][:github_authorized_keys]	    = []
