e61b65d59239aa5011a9aa0ba16955c9a6e38c7d
Pirate Dunbar <yarr@piratedunbar.com>
2013-12-27 04:06:28 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'yarr@piratedunbar.com'
 license          'All rights reserved'
 description      'Installs/Configures base'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.0.9'
 
 depends "apt"
 depends "timezone-ii"
