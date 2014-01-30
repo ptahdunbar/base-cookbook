66d159d274bd11ec97a377eb73289c447df9eb55
Pirate Dunbar <yarr@piratedunbar.com>
2014-01-30 01:10:11 -0500

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,4 +20,5 @@ include_recipe 'git'
 include_recipe 'subversion'
 include_recipe 'postfix'
 include_recipe 'ffmpeg'
+include_recipe 'yasm'
 include_recipe 'sudo' if ! node['authorization']['sudo']['users'].empty?
