0ec689916ff6baf181da8d751fad88bf5031e3a4
Ptah Dunbar <yarr@piratedunbar.com>
2014-01-31 15:04:27 -0500

    Make ffmpeg optional; speeds up chef provision

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,5 +19,4 @@ include_recipe 'vim'
 include_recipe 'git'
 include_recipe 'subversion'
 include_recipe 'postfix'
-include_recipe 'ffmpeg'
 include_recipe 'sudo' if ! node['authorization']['sudo']['users'].empty?
